740528a5e61434dbe4baa8a747a000f3d78b74bc
peu <peugomes97@gmail.com>
2024-05-16 14:57:47 -0300

    changes on package.json

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,6 @@ import { app, server } from './socket/socket.js';
 
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -23,15 +22,48 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")))
-
-app.get("*", (req,res)=>{
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
-})
 
 server.listen(PORT, () => {
     connectToMongoDB();
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
+
+// import express from 'express';
+// import dotenv from 'dotenv';
+// import path from 'path';
+
+// import authRoutes from './routes/authRoute.js';
+// import messageRoutes from './routes/messageRoutes.js';
+// import userRoutes from './routes/userRoutes.js';
+// import connectToMongoDB from './db/connectToMongoDB.js';
+// import cookieParser from 'cookie-parser';
+// import { app, server } from './socket/socket.js';
+
+// const PORT = process.env.PORT || 5000;
+
+// const __dirname = path.resolve();
+
+// dotenv.config();
+
+
+// app.use(express.json()); //Parse incoming request with json payload.
+// app.use(cookieParser());
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/messages", messageRoutes);
+// app.use("/api/users", userRoutes);
+
+// app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+// app.get("*", (req,res)=>{
+//     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+// })
+
+// server.listen(PORT, () => {
+//     connectToMongoDB();
+//     console.log(`Server running on http://localhost:${PORT}`);
+// });
 
 
