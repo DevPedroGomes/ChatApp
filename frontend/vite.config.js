9206a0f8b0d971b5650e802ad8bdfaaa21fea792
peu <peugomes97@gmail.com>
2024-05-23 16:45:50 -0300

    Deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port:3000,
+    port: 3000,
     proxy: {
-      "/api":{
-        target: process.env.TARGET || "https://localhost:5000", //or regular local path
+      '/api': {
+        target: process.env.TARGET || 'http://localhost:5000', // Use HTTP para desenvolvimento local
+        changeOrigin: true,
+      },
+      '/socket.io': {
+        target: process.env.TARGET || 'http://localhost:5000',
+        ws: true, // Habilitar proxy para WebSocket
+        changeOrigin: true,
       },
     },
   },
-})
+});
